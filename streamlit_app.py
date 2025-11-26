33059127dc786fc39a1a195a50005fdd83122287
Matt Matt <84437934+Matthewmaps@users.noreply.github.com>
2025-11-25 22:45:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,41 @@ st.markdown('''
 st.sidebar.title('📍Navigation')
 page = st.sidebar.radio('Go to',
                         ['🏠 Home', '👔 About', '💼 projects', '⚒️ Skills', '📝 Resume', '📩 Contact' ])
+
+# Home Page
+if page == '🏠 Home':
+  st.markdown('<p class="main-header">Matthew Green</p>', unsafe_allow_html=True)
+  st.markdown('<p class="sub-header">Undergraduate Student | Medagar Evers College</p>', unsafe_allow_html=True)
+
+  # Three columns for stats 
+  col1, col2, col3 = st.columns(3)
+
+  with col1:
+    st.metric ('GPA', '3.5', '📚')
+  with col2: 
+    st.metric('Projects', '5', '💻')
+  with col3:
+    st.metric('skills', '10+', '🚀')
+
+
+  st.write('---')
+
+# Introduction with columns
+  col1, col2 = st.columns([2,1])
+  with col1:
+       st.subheader('Welcome to my digital space!👋')
+       st.write('''
+                  I am a Computer Information Systems student passionate about web development and emerging technologies. Currently learning
+                  HTML, CSS, JavaScript, and Python to build innovative solutions.
+              
+                  🎯 **Current Focus:** Pursing My Business Administration Degree
+              
+                  📚 **Currently Learning:** Internet and Emerging Technologies (CIS 211)
+              
+                  🌱 **Fun Fact:** I Can Repair electronics, such as Computers and Laptops
+              ''')
+  with col2: 
+    # Placeholder for image
+    st.image('https://raw.githubusercontent.com/avinashjairam/cis211_project1/refs/heads/main/grumpy_cat.jfif', use_column_width=True)
+
+
