48cd867bac873dc4af100dd16f3a02d79478835e
Matt Matt <84437934+Matthewmaps@users.noreply.github.com>
2025-11-25 22:10:39 -0500

    Website Project Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 # Configure the page
 st.set_page_config(
-  page_title="Your Name - Portfolio",
+    page_title="Your Name - Portfolio",
     page_icon=":book:",
     layout="wide"
+)
