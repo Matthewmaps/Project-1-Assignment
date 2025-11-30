d8700dc0e43cdeead7060715725627ed2a5d2069
Matt Matt <84437934+Matthewmaps@users.noreply.github.com>
2025-11-30 15:53:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,7 +123,7 @@ elif page == '🛠 Skills':
   skills_data = {
     'Python': 85,
     'HTML/CSS': 70,
-    JavaScript': 60,
+    'JavaScript': 60,
      'SQL': 50,
     'Technical Writing': 40
   }
